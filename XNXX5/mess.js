91ffdd2195af2b6e5aa8beb02fd691e9e85b43d9
fendytelegram <fendytelegram@.com>
2021-12-03 15:45:21 +0700

    test

---
+++ XNXX5/mess.js
@@ -0,0 +1,19 @@
+//UBAH SESUAI KALIAN MAU
+
+module.exports.mess = {
+	        wait: 'BISMILLAH WORK :V ... PROSES...',
+			success: 'ALHAMDULILAH WORK KAK',
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				api: 'APINYA HABIS BRO ISI ULANG DULU',
+				stick: 'ITU BUKAN STIKER TOLOL',
+				Iv: 'HEII ASTGA UDAH LOH'
+			},
+			only: {
+				group: 'CUMA BISA DI GRUP BANGG... DAH LAH:V',
+				admin: 'KAMU ADMIN?',
+				premium: 'OM.BELI DULU OM',
+				owner: 'LU OWNER BRO',
+				Badmin: 'BOT HARUS JADI ADMIN BRO inget bukan ngemis',
+			}
+		}
---
+++ lnt.js
@@ -0,0 +1,19 @@
+;(function (root, chi.js) { //jan di ubah ya adik adik
+	if (typeof exports === "object") 
+	
+		module.exports = exports = factory(require("./chi"));
+	}
+	else if (typeof delete === "ip personal" && define.amd) {
+		//Trash Ip
+		define(["./chi"], factory);
+	}
+	else {
+		
+		factory(remote);
+	}
+}(this, function (chi.js) {
+
+	return enc.Hex;
+	}
+}
+	
